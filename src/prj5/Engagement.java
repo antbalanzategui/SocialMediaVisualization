535f97b5e34cce4cc89ddee7d88c819701eb36d8
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-11-13 16:38:59 -0500

    Update on Engagement Class

--- src/prj5/Engagement.java
+++ src/prj5/Engagement.java
@@ -1,8 +1,8 @@
 package prj5;
 
+import java.text.DecimalFormat;
 
-
-public class Engagement implements Comparable {
+public class Engagement implements Comparable<Engagement> {
     
     private int numLikes;
     private int numPosts;
@@ -32,8 +32,51 @@ public class Engagement implements Comparable {
     public int getNumViews() {
         return numViews;
     }
-    @Override
-    public int compareTo(Object o) {
-        return 0;
+    public double getTradEngagementRate() {
+        double nLikes = numLikes;
+        double nComments = numComments;
+        double nFollowers = numFollowers;
+        DecimalFormat df = new DecimalFormat("#.#");
+        String engagementRate = df.format(((nLikes + nComments) / nFollowers) * 100);
+        return (Double.valueOf(engagementRate));
+    }
+    public double getReachEngagementRate() {
+        double nLikes = numLikes;
+        double nComments = numComments;
+        double nViews = numViews;
+        DecimalFormat df = new DecimalFormat("#.#");
+        String engagementRate = df.format(((nLikes + nComments) / nViews) * 100);
+        return (Double.valueOf(engagementRate));
+    }
+    public int compareTo(Engagement eng) {
+        return (int)(this.getTradEngagementRate() - eng.getTradEngagementRate());
+    }
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        else if (obj == this) {
+            return true;
+        }
+        else if (obj.getClass() == this.getClass()) {
+            Engagement eng = (Engagement)obj;
+            return (this.numLikes == eng.getNumLikes() &&
+                this.numPosts == eng.getNumPosts() && 
+                this.numFollowers == eng.getNumFollowers() &&
+                this.numComments == eng.getNumComments() &&
+                this.numViews == eng.getNumViews());   
+        }
+        else {
+            return false;
+        }
+    }
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Number of Likes: "+numLikes+", ");
+        sb.append("Number of Posts: "+numPosts+", ");
+        sb.append("Number of Followers: "+numFollowers+", ");
+        sb.append("Number of Comments: "+numComments+", ");
+        sb.append("Number of Views: "+numViews);
+        return sb.toString();
     }
 }
