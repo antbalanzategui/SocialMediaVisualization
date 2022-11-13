fb2b05344d09411aad38203a2ea76c76d4598012
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-11-12 21:53:21 -0500

    First Real Commit

--- src/prj5/Engagement.java
+++ src/prj5/Engagement.java
@@ -1,5 +1,33 @@
 package prj5;
 
 public class Engagement {
-
+    
+    private int numLikes;
+    private int numPosts;
+    private int numFollowers;
+    private int numComments;
+    private int numViews;
+    
+    public Engagement(int likes, int posts, int followers, int comments, int views) {
+        this.numLikes = likes;
+        this.numPosts = posts;
+        this.numFollowers = followers;
+        this.numComments = comments;
+        this.numViews = views;
+    }
+    public int getNumLikes() {
+        return numLikes;
+    }
+    public int getNumPosts() {
+        return numPosts;
+    }
+    public int getNumFollowers() {
+        return numFollowers;
+    }
+    public int getNumComments() {
+        return numComments;
+    }
+    public int getNumViews() {
+        return numViews;
+    }
 }
