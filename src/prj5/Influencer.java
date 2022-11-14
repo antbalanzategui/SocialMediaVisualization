c3de6d1cb5c039fbdf898552eaac68553a459150
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-11-14 13:19:49 -0500

    Created Documentation of Influencer class

--- src/prj5/Influencer.java
+++ src/prj5/Influencer.java
@@ -1,5 +1,18 @@
 package prj5;
 
+/**
+ * Implementation of the Influencer class
+ * 
+ *Virginia Tech Honor Code Pledge:
+ *As a Hokie, I will conduct myself with honor and integrity at all times.
+ *I will not lie, cheat, or steal, 
+ *nor will I accept the actions of those who do.
+ * 
+ * @author Antonio Balanzategui, antbalanzategui
+ * 
+ * @version 2022.11.14
+ *
+ */
 public class Influencer {
     
     private String username;
@@ -8,6 +21,24 @@ public class Influencer {
     private String mainTopic;
     private Engagement[] engagements;
     
+    /**
+     * Constructor for the Influencer class
+     * 
+     * @param user
+     * username of influencer
+     * 
+     * @param cName
+     * channelName of influencer 
+     * 
+     * @param con
+     * country of influencer
+     * 
+     * @param mTop
+     * mainTopic of influencer
+     * 
+     * @param eng
+     * engagemenets of influencer
+     */
     public Influencer(String user, String cName, String con, String mTop, Engagement[] eng) {
         this.username = user;
         this.channelName = cName;
@@ -15,22 +46,56 @@ public class Influencer {
         this.mainTopic = mTop;
         this.engagements = eng;
     }
-    
+    /**
+     * Getter for username
+     * 
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
+    /**
+     * Getter for channelName
+     * 
+     * @return channelName
+     */
     public String getChannelName() {
         return channelName;
     }
+    /**
+     * Getter for country
+     * 
+     * @return country
+     */
     public String getCountry() {
         return country;
     }
+    /**
+     * Getter for mainTopic
+     * 
+     * @return mainTopic
+     */
     public String getMainTopic() {
         return mainTopic;
     }
+    /**
+     * Getter for engagements
+     * 
+     * @return engagements
+     */
     public Engagement[] getEngagements() {
         return engagements;
     }
+    /**
+     * Method to find the engagment for a particular month 
+     * within our engagement array
+     * 
+     * @param month
+     * month requested 
+     * 
+     * @return the engagement of that particular month
+     * null if month is not found
+     */
     public Engagement getEngagementForMonth(MonthEnum month) {
         for (int i = 0; i < engagements.length; i++) {
             if (engagements[i].getMonth() == month) {
@@ -39,6 +104,17 @@ public class Influencer {
         }
         return null;
     }
+    /**
+     * Method to check equality of
+     * two influencer objects
+     * 
+     * @param obj
+     * object of comparison to "this"
+     * 
+     * @return true if equal,
+     * false otherwise
+     * 
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -66,7 +142,12 @@ public class Influencer {
             return false;
         }
     }
-    
+    /**
+     * Method to display data of influencer 
+     * object as a string
+     * 
+     * @return String of influencer object
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("Username: "+username+", ");
