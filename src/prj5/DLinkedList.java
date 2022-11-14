8c0eb6b85f909ce741bc5306b31f8f4ff18762c0
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-11-14 16:24:08 -0500

    Creation of DLinkedList class

---
+++ src/prj5/DLinkedList.java
@@ -0,0 +1,58 @@
+package prj5;
+
+import list.ListInterface;
+
+@SuppressWarnings("hiding")
+public class DLinkedList<Influencer> implements ListInterface<Influencer> {
+
+    @Override
+    public void add(Influencer arg0) {
+        
+    }
+
+    @Override
+    public void add(int arg0, Influencer arg1) {
+        
+    }
+
+    @Override
+    public void clear() {
+        
+    }
+
+    @Override
+    public boolean contains(Influencer arg0) {
+        return false;
+    }
+
+    @Override
+    public Influencer getEntry(int arg0) {
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public Influencer remove(int arg0) {
+        return null;
+    }
+
+    @Override
+    public Influencer replace(int arg0, Influencer arg1) {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return null;
+    }
+
+}
