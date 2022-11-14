2ad7c33f00b674ad68b070c215e31736db485dee
Antonio Balanzategui <antbalanzategui@vt.edu>
2022-11-13 19:54:39 -0500

    Updated Tests

--- src/prj5/EngagementTest.java
+++ src/prj5/EngagementTest.java
@@ -8,10 +8,14 @@ public class EngagementTest extends TestCase {
     private Engagement eng2;
     
     public void setUp() {
-        eng1 = new Engagement(7, 9, 8, 100, 5);
-        eng2 = new Engagement(6, 9, 80, 1000, 50);
+        eng1 = new Engagement("Jan", 7, 9, 8, 100, 5);
+        eng2 = new Engagement("Feb", 6, 9, 80, 1000, 50);
     }
     
+    public void testGetMonth() {
+        assertEquals(eng1.getMonth(), "Jan");
+        assertEquals(eng2.getMonth(), "Feb");
+    }
     public void testGetNumLikes() {
         assertEquals(eng1.getNumLikes(), 7);
         assertEquals(eng2.getNumLikes(), 6);
@@ -37,5 +41,16 @@ public class EngagementTest extends TestCase {
         assertEquals(eng1.getNumViews(), 5);
         assertEquals(eng2.getNumViews(), 50);
     }
+    
+    public void testGetTradEngagementRate() {
+        assertEquals(eng1.getTradEngagementRate(), 1337.5, 0.0001);
+        assertEquals(eng2.getTradEngagementRate(), 1257.5, 0.0001);
+    }
+    
+    public void testGetReachEngagementRate() {
+        assertEquals(eng1.getReachEngagementRate(), 2140.0, 0.0001);
+        assertEquals(eng2.getReachEngagementRate(), 2012.0, 0.0001);
+    }
+    
 
 }
