de629a49a4269bfc3602a917c07bbf5289537bc2
Lukyan Sukhachevskyi <lukyan2004@gmail.com>
2022-11-15 00:09:31 -0500

    Add(T) Implemented

--- src/prj5/DLinkedList.java
+++ src/prj5/DLinkedList.java
@@ -1,9 +1,35 @@
+// Virginia Tech Honor Code Pledge:
+//
+// As a Hokie, I will conduct myself with honor and integrity at all times.
+// I will not lie, cheat, or steal, nor will I accept the actions of
+// those who do.
+// -- Lukyan Sukhachevskyi (lukyan)
+
 package prj5;
 
 import list.ListInterface;
 
-@SuppressWarnings("hiding")
+/**
+ * Project: socialmediavisualization
+ * Class: DLNode
+ *
+ * @param <Influencer> the Influencer parameter
+ * @author Lukyan Sukhachevskyi (lukyan)
+ * @version 2022.11.14
+ */
 public class DLinkedList<Influencer> implements ListInterface<Influencer> {
+
+    private DLNode<Influencer> head;
+    private DLNode<Influencer> tail;
+    private int size;
+
+
+    public DLinkedList(){
+
+        head = null;
+        tail = null;
+        size = 0;
+    }
 
     //find the index of an influencer
     public int getIndex(Influencer influencer){
@@ -12,18 +38,31 @@ public class DLinkedList<Influencer> implements ListInterface<Influencer> {
     }
 
     @Override
-    public void add(Influencer arg0) {
-        
+    public void add(Influencer influencer) {
+
+        DLNode<Influencer> newNode = new DLNode<Influencer>(influencer);
+
+        if(head == null){
+
+            head = tail = newNode;
+            size++;
+        }else{
+
+            newNode.setPrevious(tail);
+            tail.setNext(newNode);
+            tail = tail.getNext();
+            size++;
+        }
     }
 
     @Override
-    public void add(int arg0, Influencer arg1) {
+    public void add(int index, Influencer influencer) {
         
     }
 
     @Override
     public void clear() {
-        
+
     }
 
     //RETURN TRUE IF USERNAME MATCHES (not .equals) - Lukyan
